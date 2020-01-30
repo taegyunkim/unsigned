5a8f2b7d5b82c09f3be3757d381abe3f7d581ead
Taegyun Kim <k.taegyun@gmail.com>
2020-01-30 15:36:22 -0500

    Add comments.

--- overflow.cpp
+++ overflow.cpp
@@ -4,7 +4,7 @@ int main(int argc, const char *argv[]) {
   uint8_t a = 0x80;
   uint8_t b = 0x80;
   uint8_t c = a + b;
-  printf("%u\n", c);
-  std::cout << c << std::endl;
+  printf("%u\n", c); // prints 0
+  std::cout << c << std::endl; // doesn't print anything?
   return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,6 @@ fn main() {
     let c: u8 = a + b;
     println!("{}", c);
 }
+
+// This program will panic at line 4, with an error message as following
+// thread 'main' panicked at 'attempt to add with overflow'
